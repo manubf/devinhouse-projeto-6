ee34d326b5d0b4b41094f90729873e12d38c49a2
abrugiolo <abrugiolosz@gmail.com>
2021-07-21 08:09:15 -0300

    Adiciona Yup

--- Projetos/FrontEnd_/src/pages/Home/fragments/CreateEditProcess/constants.js
+++ Projetos/FrontEnd_/src/pages/Home/fragments/CreateEditProcess/constants.js
@@ -2,11 +2,13 @@ const hoje = new Date();
 const ano = hoje.getFullYear();
 let mes = JSON.stringify(hoje.getMonth() + 1);
 if (mes < 10) {
-  mes = "0" + mes;
+	mes = "0" + mes;
 }
 let dia = hoje.getDate();
 if (dia < 10) {
-  dia = "0" + dia;
+	dia = "0" + dia;
 }
 
 export const today = `${dia}/${mes}/${ano}`;
+
+export const currentYear = new Date().getFullYear();
