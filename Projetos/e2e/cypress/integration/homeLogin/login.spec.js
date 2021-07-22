5e287abbfccf10d2df0b72a3d9d67b67ca99b8ec
manubf <emanuellebf@gmail.com>
2021-07-22 13:01:26 -0300

    teste e2e login keycloak

---
+++ Projetos/e2e/cypress/integration/homeLogin/login.spec.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+describe("Login", () => {
+  it("efetuar login", () => {
+    //Acessar a página
+    cy.visit("/home");
+
+    //Informar login senha
+    cy.get("input[name=username]").type("user");
+    cy.get("input[name=password]").type("123");
+
+    //Clicar no botão enviar
+    cy.get("input[type=submit]").click();
+
+    // Verificar se redirecionou para a /home novamente
+    cy.url().should("include", "/home");
+  });
+});
